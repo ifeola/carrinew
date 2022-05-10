32e8a438b624557436dc5f9d5ba61cf283d907db
DS Ifeola <nomorelimit11@gmail.com>
2022-05-10 17:38:06 +0100

    My new commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector("#nav_items");
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+closeBtn.addEventListener("click", () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('header').classList.toggle('window-scroll', window.scrollY > 0);
+})
